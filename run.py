f795a3f36b2383cefa8bb62a01e4590829d87d85
JackyChen0725 <jacky789012@gmail.com>
2022-12-24 19:31:59 +0800

    modify run.py and .sh, add gt

--- run.py
+++ run.py
@@ -159,112 +159,62 @@ class UNet(nn.Module):
         logits = self.fc(x)
         return logits
 
-"""
 ## Resnet
-class Bottleneck(nn.Module):
-    # __init__
-    #     in_channel: 残差块输入通道数
-    #     out_channel: 残差块输出通道数
-    #     stride: 卷积步长
-    #     downsample: 在_make_layer函数中赋值, 用于控制shortcut图片下采样 H/2 W/2
-    # expansion = 4   # 残差块第3个卷积层的通道膨胀倍率
-    def __init__(self, in_channel, out_channel, stride=1, downsample=None):
-        super(Bottleneck, self).__init__()
-        
-        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel, kernel_size=1, stride=1, bias=False)   # H,W不变。C: in_channel -> out_channel
-        self.bn1 = nn.BatchNorm2d(num_features=out_channel)
-        self.conv2 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel, kernel_size=3, stride=stride, bias=False, padding=1)  # H/2，W/2。C不变
-        self.bn2 = nn.BatchNorm2d(num_features=out_channel)
-        self.conv3 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel*self.expansion, kernel_size=1, stride=1, bias=False)   # H,W不变。C: out_channel -> 4*out_channel
-        self.bn3 = nn.BatchNorm2d(num_features=out_channel*self.expansion)
+class Block0(nn.Module):
+    def __init__(self, ic, oc, ks):
+        super(Block0, self).__init__()
         self.relu = nn.ReLU(inplace=True)
-        
-        self.downsample = downsample
-        
+        self.path1 = nn.Sequential(
+            nn.Conv2d(ic, oc, 1, bias=False), nn.BatchNorm2d(oc))
+        self.path2 = nn.Sequential(
+            nn.Conv2d(ic, oc, ks, padding=ks//2, bias=False), nn.BatchNorm2d(oc), self.relu,
+            nn.Conv2d(oc, oc, ks, padding=ks//2, bias=False), nn.BatchNorm2d(oc))
     def forward(self, x):
-        identity = x    # 将原始输入暂存为shortcut的输出
-        if self.downsample is not None:
-            identity = self.downsample(x)   # 如果需要下采样，那么shortcut后:H/2，W/2。C: out_channel -> 4*out_channel(见ResNet中的downsample实现)
-        
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-        
-        out = self.conv3(out)
-        out = self.bn3(out)
-        
-        out += identity     # 残差连接
-        out = self.relu(out)
-        
-        return out
-    
-class ResNet(nn.Module):
-    # __init__
-    #     block: 堆叠的基本模块
-    #     block_num: 基本模块堆叠个数,是一个list,对于resnet50=[3,4,6,3]
-    #     num_classes: 全连接之后的分类特征维度
-    # _make_layer
-    #     block: 堆叠的基本模块
-    #     channel: 每个stage中堆叠模块的第一个卷积的卷积核个数, 对resnet50分别是:64,128,256,512
-    #     block_num: 当期stage堆叠block个数
-    #     stride: 默认卷积步长
-    def __init__(self, block, block_num, num_classes=1000):
-        super(ResNet, self).__init__()
-        self.in_channel = 64    # conv1的输出维度
-        
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=self.in_channel, kernel_size=7, stride=2, padding=3, bias=False)     # H/2,W/2。C:3->64
-        self.bn1 = nn.BatchNorm2d(self.in_channel)
+        return self.relu(self.path1(x) + self.path2(x))
+
+class Block1(nn.Module):
+    def __init__(self, c, ks):
+        super(Block1, self).__init__()
         self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)     # H/2,W/2。C不变
-        self.layer1 = self._make_layer(block=block, channel=64, block_num=block_num[0], stride=1)   # H,W不变。downsample控制的shortcut，out_channel=64x4=256
-        self.layer2 = self._make_layer(block=block, channel=128, block_num=block_num[1], stride=2)  # H/2, W/2。downsample控制的shortcut，out_channel=128x4=512
-        self.layer3 = self._make_layer(block=block, channel=256, block_num=block_num[2], stride=2)  # H/2, W/2。downsample控制的shortcut，out_channel=256x4=1024
-        self.layer4 = self._make_layer(block=block, channel=512, block_num=block_num[3], stride=2)  # H/2, W/2。downsample控制的shortcut，out_channel=512x4=2048
+        self.path = nn.Sequential(
+            nn.Conv2d(c, c, ks, padding=ks//2, bias=False), nn.BatchNorm2d(c), self.relu,
+            nn.Conv2d(c, c, ks, padding=ks//2, bias=False), nn.BatchNorm2d(c))
+    def forward(self, x):
+        return self.relu(x + self.path(x))
+
+class Resnet(nn.Module):
+    def __init__(self, num_class=1922, in_ch = 16):
+        super().__init__()
+        d, block_num = 8, 7
+        ds = [1] + [d*2**i for i in range(block_num)]
+        tmp = []
+        for i in range(block_num):
+            tmp.append(Block0(ds[i], ds[i+1], 3))
+            tmp.append(nn.MaxPool2d(3, stride=2, padding=1))
+            tmp.append(Block1(ds[i+1], 3))
+            #tmp.append(Block1(ds[i+1], 3))
+        self.layer = nn.Sequential(*tmp)
         
-        self.avgpool = nn.AdaptiveAvgPool2d((1,1))  # 将每张特征图大小->(1,1)，则经过池化后的输出维度=通道数
-        self.fc = nn.Linear(in_features=512*block.expansion, out_features=num_classes)
+        size = 512 // 2**block_num
+        self.fc = nn.Linear(d*(2**block_num)//2*size*size , num_class)
         
-        for m in self.modules():    # 权重初始化
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                
-    def _make_layer(self, block, channel, block_num, stride=1):
-        downsample = None   # 用于控制shorcut路的
-        if stride != 1 or self.in_channel != channel*block.expansion:   # 对resnet50：conv2中特征图尺寸H,W不需要下采样/2，但是通道数x4，因此shortcut通道数也需要x4。对其余conv3,4,5，既要特征图尺寸H,W/2，又要shortcut维度x4
-            downsample = nn.Sequential(
-                nn.Conv2d(in_channels=self.in_channel, out_channels=channel*block.expansion, kernel_size=1, stride=stride, bias=False), # out_channels决定输出通道数x4，stride决定特征图尺寸H,W/2
-                nn.BatchNorm2d(num_features=channel*block.expansion))
-            
-        layers = []  # 每一个convi_x的结构保存在一个layers列表中，i={2,3,4,5}
-        layers.append(block(in_channel=self.in_channel, out_channel=channel, downsample=downsample, stride=stride)) # 定义convi_x中的第一个残差块，只有第一个需要设置downsample和stride
-        self.in_channel = channel*block.expansion   # 在下一次调用_make_layer函数的时候，self.in_channel已经x4
-        
-        for _ in range(1, block_num):  # 通过循环堆叠其余残差块(堆叠了剩余的block_num-1个)
-            layers.append(block(in_channel=self.in_channel, out_channel=channel))
-            
-        return nn.Sequential(*layers)   # '*'的作用是将list转换为非关键字参数传入
     
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
+        x = self.layer(x)
         
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
+        x = torch.flatten(x,1)
+        #print(x.size())
         x = self.fc(x)
-        
+        #x = self.fc3(x)
         return x
-"""
+        
+    def toh(self, model):
+        for child_name, child in model.named_children():
+            if isinstance(child, nn.ReLU):
+                setattr(model, child_name, nn.Softplus())
+            else:
+                self.toh(child)
+
 ## Resnest
 from resnest.torch import resnest50
 
@@ -272,7 +222,7 @@ from resnest.torch import resnest50
 
 train_loss_list = []
 def train(model, dataloader, num_epoch, device, criterion, optimizer, save_path):
-    best_loss = 9999
+    best_loss = 50
     # cnt = 0
     model.train()
     print(f'Training...')
@@ -397,10 +347,10 @@ def main():
     # parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("basedir", help="Base path of train/testing data", type=str)
-    parser.add_argument("model", choices=['Unet', 'Resnet', 'Resnest'], help='which model architecture to use', type=str)
+    parser.add_argument("model", choices=['Unet', 'Resnest', 'gt'], help='which model architecture to use', type=str)
     # parser.add_argument("savedir", choose=['Unet_output', 'Resnet_output', 'Transformer_output'], help="Path to save state dict or predicted csvs",
     #                 type=Path)
-    parser.add_argument("--ckpt", help="Path to ckpt file, eg. output/Unet/model.ckpt", type=str)
+    parser.add_argument("--ckpt", help="Path to ckpt file, eg. Unet/model.ckpt", type=str)
     parser.add_argument("--do_predict", help="Do predict",
                     action='store_true')
 
@@ -468,7 +418,7 @@ def main():
         print('Done')
     
     # Create model
-    models = {'Unet': UNet(), 'Resnest': resnest50(pretrained=False, dilated=True, num_classes=1922)} # define which model to use
+    models = {'Unet': UNet(), 'Resnest': resnest50(pretrained=False, dilated=True, num_classes=1922), 'gt': Resnet()} # define which model to use
     model = models[args.model]
     model.to(device)
 
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-python3 run.py 3dcv_dataset Resnest \
---num_epoch 1000 \
+python3 run.py 3dcv_dataset gt \
+--num_epoch 3500 \
 --batch_size 8 \
 --lr 1e-4 \
 --weight_decay 1e-4 \
